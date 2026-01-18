9bdf9466c7ea92aa7678958c65052432aa780129
sinv39 <14873201+sinv39@user.noreply.gitee.com>
2026-01-18 10:56:56 +0800

    [V1.0]能跑就是好代码

---
+++ frontend/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <h1>连音成歌</h1>
+    <GameBoard />
+    <div class="links">
+      <a href="/admin.html">管理页面</a>
+    </div>
+  </div>
+</template>
+
+<script>
+import GameBoard from './components/GameBoard.vue';
+
+export default {
+  name: 'App',
+  components: {
+    GameBoard
+  }
+};
+</script>
+
